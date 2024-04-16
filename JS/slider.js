0666a20da27e8aa1c552b8a6a464883f5de5840c
Dcontreras17 <166152420+Dcontreras17@users.noreply.github.com>
2024-04-16 10:30:58 -0500

    registro

--- JS/slider.js
+++ JS/slider.js
@@ -32,3 +32,21 @@
     }
     
     })();
+
+    document.getElementById('registroEquipoForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Evitar que el formulario se envíe
+        
+        // Obtener los valores de los campos del formulario
+        var nombreEquipo = document.getElementById('nombreEquipo').value;
+        var deporte = document.getElementById('deporte').value;
+        var capitan = document.getElementById('capitan').value;
+        var telefono = document.getElementById('telefono').value;
+        
+        // Aquí puedes agregar la lógica para enviar los datos a tu servidor o realizar otras acciones con ellos
+        // Por ahora, simplemente los mostraremos en la consola
+        console.log("Nombre del Equipo: " + nombreEquipo);
+        console.log("Deporte: " + deporte);
+        console.log("Capitán: " + capitan);
+        console.log("Teléfono de Contacto: " + telefono);
+    });
+    
