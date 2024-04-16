899b5160af05f160c1cfc52e34314580509cff30
Dcontreras17 <166152420+Dcontreras17@users.noreply.github.com>
2024-04-16 17:17:38 -0500

    from

---
+++ form.php
@@ -0,0 +1,23 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$depote = $_POST['deporte'];
+$capitan = $_POST['capitan'];
+$telefono = $_POST['telefono'];
+$mail = $_POST['mail'];
+
+
+$header = "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviada por ". $nombre.  " ,\r\n ";
+$mensaje = "Su E-mail es: " .$mail. "\r\n";
+$mensaje = "Enviado el:  " .date('d/m/Y', time());
+
+$para = 'dcontreras44@itfip.edu.co';
+$asunto = 'sitio web de ItfipSport';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+header('Locatio:registro.html')
+
+
+?>
