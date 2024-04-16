732e7da40f35820b4427d9d68a943a73a419ef2a
Dcontreras17 <166152420+Dcontreras17@users.noreply.github.com>
2024-04-16 17:28:13 -0500

    Update form.php

--- form.php
+++ form.php
@@ -1,23 +1,21 @@
 <?php
 
 $nombre = $_POST['nombre'];
-$depote = $_POST['deporte'];
+$deporte = $_POST['deporte'];
 $capitan = $_POST['capitan'];
 $telefono = $_POST['telefono'];
 $mail = $_POST['mail'];
 
+$header = "From: " . $mail . "\r\n";
+$header .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-$header = "Content-Type: text/plain";
-
-$mensaje = "Este mensaje fue enviada por ". $nombre.  " ,\r\n ";
-$mensaje = "Su E-mail es: " .$mail. "\r\n";
-$mensaje = "Enviado el:  " .date('d/m/Y', time());
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su E-mail es: " . $mail . "\r\n";
+$mensaje .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'dcontreras44@itfip.edu.co';
 $asunto = 'sitio web de ItfipSport';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
-header('Location:registro.html')
-
-
+header('Location: registro.html');
 ?>
